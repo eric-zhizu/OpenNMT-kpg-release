2e9ec96ad761aa394e781542a61468075788cdfb
Ubuntu <ubuntu@ip-172-31-17-44.us-east-2.compute.internal>
2021-11-24 04:32:03 +0000

    Revise script

--- script/empirical_study/diverse/transformer/transformer-presabs-kptimes.sh
+++ script/empirical_study/diverse/transformer/transformer-presabs-kptimes.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Run the job
-export CONFIG_PATH="script/empirical_study/diverse/transformer/transformer-presabs-kp20k.yml"
+export CONFIG_PATH="script/empirical_study/diverse/transformer/transformer-presabs-kptimes.yml"
 cmd="python train.py -config $CONFIG_PATH"
 
 echo $CONFIG_PATH
