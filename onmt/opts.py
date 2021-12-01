f4dfc143a2db1f29529b7e45b62f0a5b450ea393
eric-zhizu <zhou.eric.y@gmail.com>
2021-12-01 00:42:01 -0500

    Edit configs for bart training

--- onmt/opts.py
+++ onmt/opts.py
@@ -306,12 +306,12 @@ def model_opts(parser):
 
     group.add('--encoder_type', '-encoder_type', type=str, default='rnn',
               choices=['rnn', 'brnn', 'ggnn', 'mean', 'transformer', 'cnn',
-                       'transformer_lm', 'huggingface', 'fairseq_bart', 'pretrained'],
+                       'transformer_lm', 'huggingface', 'fairseq_bart', 'pretrained', 'bart'],
               help="Type of encoder layer to use. Non-RNN layers "
                    "are experimental. Options are "
                    "[rnn|brnn|ggnn|mean|transformer|cnn|transformer_lm].")
     group.add('--decoder_type', '-decoder_type', type=str, default='rnn',
-              choices=['rnn', 'transformer', 'cnn', 'transformer_lm', 'huggingface', 'fairseq_bart', 'pretrained'],
+              choices=['rnn', 'transformer', 'cnn', 'transformer_lm', 'huggingface', 'fairseq_bart', 'pretrained', 'bart'],
               help="Type of decoder layer to use. Non-RNN layers "
                    "are experimental. Options are "
                    "[rnn|transformer|cnn|transformer].")
