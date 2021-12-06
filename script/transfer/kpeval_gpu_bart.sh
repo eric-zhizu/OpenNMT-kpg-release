2947331920eb86ed8bffab41ab8f0c4c4de273a5
Ubuntu <ubuntu@ip-172-31-38-55.us-east-2.compute.internal>
2021-12-06 15:20:02 +0000

    Debug eval script for bart

--- script/transfer/kpeval_gpu_bart.sh
+++ script/transfer/kpeval_gpu_bart.sh
@@ -14,7 +14,7 @@
 #SBATCH --time={days}-00:00:00 # 6 days walltime in dd-hh:mm format
 #SBATCH --qos=long
 
-cmd="python kp_gen_eval_transfer.py -config config/transfer_kp/infer/keyphrase-one2seq.yml -tasks pred -data_dir /home/ubuntu/kptimes_NYTimes500 -exp_root_dir /home/ubuntu/transformer-presabs-kptimes-bart-trial-01 -testsets kptimes_NYTimes500 -splits test -batch_size 32 -beam_size 1 -max_length 32 -beam_terminate full --step_base 1 --data_format jsonl --pred_trained_only -gpu 0"
+cmd="python kp_gen_eval_transfer.py -config config/transfer_kp/infer/keyphrase-one2seq.yml -tasks pred -data_dir /home/ubuntu/ -exp_root_dir /home/ubuntu/transformer-presabs-kptimes-bart-trial-01 -testsets kptimes_NYTimes500 -splits test -batch_size 32 -beam_size 1 -max_length 32 -beam_terminate full --step_base 1 --data_format jsonl --pred_trained_only -gpu 0 -onepass"
 echo $cmd
 echo $PWD
 $cmd
