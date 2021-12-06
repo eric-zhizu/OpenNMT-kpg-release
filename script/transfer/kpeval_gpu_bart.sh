2e837efd9da0f3983eee39f6875abdaa1ea36be8
eric-zhizu <zhou.eric.y@gmail.com>
2021-12-06 09:53:58 -0500

    Add new eval script for bart

---
+++ script/transfer/kpeval_gpu_bart.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#SBATCH --cluster=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --account=hdaqing
+
+#SBATCH --partition={partition}
+
+#SBATCH --job-name={job_name}
+#SBATCH --output={slurm_output_dir}/{job_name}.out
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16GB
+#SBATCH --time={days}-00:00:00 # 6 days walltime in dd-hh:mm format
+#SBATCH --qos=long
+
+cmd="python kp_gen_eval_transfer.py -config config/transfer_kp/infer/keyphrase-one2seq.yml -tasks pred -data_dir /home/ubuntu/kptimes_NYTimes500 -exp_root_dir /home/ubuntu/transformer-presabs-kptimes-bart-trial-01 -testsets kptimes_NYTimes500 -splits test -batch_size 32 -beam_size 1 -max_length 32 -beam_terminate full --step_base 1 --data_format jsonl --pred_trained_only -gpu 0"
+echo $cmd
+echo $PWD
+$cmd
